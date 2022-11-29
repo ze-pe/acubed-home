8ed2f6c7d91482ad5a9b2772ededea97410ceb39
ze-pe <83299059+ze-pe@users.noreply.github.com>
2022-11-28 19:48:34 -0500

    increase padding of h2 and paragraph text

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -2,7 +2,7 @@
 
 <div class="contact" id="contact">
   <h2>Get Involved</h2>
-  <p>Want to get Involved? Want to provide feedback?</p>
+  <p>Want to get involved? Want to provide feedback?</p>
   <p>Fill out the form below to get in contact with us.</p>
   <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfynGDAGn5yMX5LjyVCN98s9Cmx1M3hItnamqN4jmdYEgdavA/viewform?embedded=true" width="100%" height="600" frameborder="0" marginheight="0" marginwidth="0" class="ui-droppable" title="Google Form">Loading…</iframe>
 </div>
@@ -30,10 +30,14 @@
   h2 {
     font-size: 60px;
     line-height: 1em;
+    padding: 0 auto;
+    margin-bottom: .3em;
   }
 
   p {
-    line-height: 2rem;
+    font-size: 1.5em;
+    line-height: 2em;
+    padding: 0 auto;
   }
 
   iframe {
