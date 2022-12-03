899bc008f3bfc3fd3abb8969879edd76975840b7
ze-pe <83299059+ze-pe@users.noreply.github.com>
2022-12-02 21:41:36 -0500

    add new components

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,13 @@
 <script>
   import Header from './components/Header.svelte';
   import Footer from './components/Footer.svelte';
+  import Footer2 from './components/Footer2.svelte';
   import Hero from './components/Hero.svelte';
   import ThreeA from './components/ThreeA.svelte';
   import Enigma from './components/Enigma.svelte';
   import Team from './components/Team.svelte';
+  import OurTeam from './components/OurTeam.svelte';
+  import Buttons from './components/Buttons.svelte';
   import Partners from './components/Partners.svelte'
   import Contact from './components/Contact.svelte';
 
@@ -20,13 +23,20 @@
   <ThreeA />
   <Partners />
   <Team />
-  <Footer />
+  <Buttons />
+  <Footer2 />
   {/if}
 
   {#if $activeView === 'form'}
   <Header />
   <Contact />
-  <Footer />
+  <Footer2 />
+  {/if}
+
+  {#if $activeView === 'ourTeam'}
+  <Header />
+  <OurTeam />
+  <Footer2 />
   {/if}
 
 </main>
