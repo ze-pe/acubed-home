f2f7dc98307691ecdb781b499ab64a91d03ed545
ze-pe <83299059+ze-pe@users.noreply.github.com>
2022-12-02 21:44:55 -0500

    display original footer styling

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,4 +1,4 @@
-<!-- <footer>
+<footer>
   <img src="./assets/ACubedLogo.png" alt="A Cubed Design Logo">
   <p class="dark-text">Copyright 2022 Accessible Affordable Accelerated Design LLC</p>
   <div class="icon-attributions">Icon Attributions
@@ -8,7 +8,7 @@
       <a href="https://www.flaticon.com/free-icons/rocket" title="rocket icons">Rocket icon by Freepik - Flaticon</a>
     </span>
   </div>
-  SVGs copied from acubed site
+  <!-- SVGs copied from acubed site -->
   <a href="https://www.facebook.com/A-Cubed-Design-104416965644412" target="_blank">
     <svg class="svg-content fb" viewBox="0 0 112 112" x="0" y="0" id="svg-7e29"><circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle><path fill="#FFFFFF" d="M73.5,31.6h-9.1c-1.4,0-3.6,0.8-3.6,3.9v8.5h12.6L72,58.3H60.8v40.8H43.9V58.3h-8V43.9h8v-9.2
       c0-6.7,3.1-17,17-17h12.5v13.9H73.5z"></path></svg>
@@ -121,153 +121,5 @@
     visibility: visible;
     opacity: 1;
   }
-</style> -->
-
-<!-- New Footer Begins Here -->
-<script>
-  import {activeView} from '../stores';
-  
-  function formHandler() {
-    $activeView = 'form';
-  }
-  
-  function mainHandler() {
-    $activeView = 'main';
-  }
-</script>
-
-<footer class="footer">
-  <div class="container grid">
-      <div>
-        <img src="./assets/ACubedLogo.png" alt="A Cubed Design Logo">
-      </div>
-      <nav>
-          <ul>
-              <li><a href="/" on:click={mainHandler}>About</a></li>
-              <li><a href="#team" on:click={mainHandler}>Team</a></li>
-              <li><a href="#contact" on:click={formHandler}>Contact</a></li>
-          </ul>
-      </nav>
-      <div class="social">
-          <a href="https://www.linkedin.com/company/84838459/"><i class="fab fa-linkedin fa-2x"></i></a>
-          <a href="https://www.facebook.com/A-Cubed-Design-104416965644412"><i class="fab fa-facebook fa-2x"></i></a>
-          <a href="https://www.instagram.com/acubeddesign/"><i class="fab fa-instagram fa-2x"></i></a>
-          <a href="#"><i class="fab fa-twitter fa-2x"></i></a>
-      </div>
-  </div>
-  <div class="icon-attributions">Icon Attributions
-    <span class="attribution-links">
-      <a href="https://www.flaticon.com/free-icons/braille" title="braille icons">Braille icon by Freepik - Flaticon | </a>
-      <a href="https://www.flaticon.com/free-icons/loan" title="loan icons">Loan icon by Smashicons - Flaticon | </a>
-      <a href="https://www.flaticon.com/free-icons/rocket" title="rocket icons">Rocket icon by Freepik - Flaticon</a>
-    </span>
-  </div>
-  <p>Copyright &copy; 2022 Accessible Affordable Accelerated Design LLC</p>
-</footer>
-
-<style>
-/* Footer */
-.container {
-  max-width: 1800px;
-  margin: 0 auto;
-  overflow: auto;
-}
-
-.grid {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  /* gap: 24px; */
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-}
-
-img {
-  margin-right: auto;
-  width: 90px;
-}
-
-ul {
-  list-style-type: none;
-  margin-bottom: 1.5em;
-}
-
-a {
-  text-decoration: none;
-  color: #fff;
-}
-
-a:hover {
-  color: hsl(282 98% 49% / 0.8);
-}
-
-p {
-  font-size: 1em;
-}
-
-span a {
-  color: blue;
-}
- 
-.footer {
-  background: hsl(281 94% 21%);
-  padding: 2rem 0;
-}
-
-.footer .social a {
-  margin: 0 10px;
-}
-
-.fa-linkedin:hover {
-  color: #0E76A8;
-}
-
-.fa-facebook:hover {
-  color: #1773EA;
-}
-
-.fa-instagram:hover {
-  color: #B32E87;
-}
-
-.fa-twitter:hover {
-  color: #1C9CEA;
-}
-
-.icon-attributions {
-  color: #fff;
-  position: relative;
-  font-size: .8em;
-  margin-top: 4em;
-}
-
-.icon-attributions .attribution-links {
-  visibility: hidden;
-  width: 300px;
-  color: black;
-  text-align: center;
-  border-radius: 6px;
-  padding: 5px 0;
-  position: absolute;
-  z-index: 100;
-  border: 3px solid rgb(0, 0, 0);
-  bottom: 100%;
-  left: 50%;
-  margin-left: -150px;
-  background-color:white;
-  opacity: 0;
-  transition: opacity 1s;
-}
-
-.icon-attributions:hover .attribution-links {
-  visibility: visible;
-  opacity: 1;
-}
-
-@media (max-width: 768px) {
-  .grid {
-    grid-template-columns: 1fr;
-    grid-template-rows: 1fr;
-  }
-}
 </style>
+
